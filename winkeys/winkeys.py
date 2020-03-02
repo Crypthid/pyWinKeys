a9cf516fed4b86eba6d1ed6fa9e0e13be14a4aaa
Daniel Månsson <retaliator95@gmail.com>
2020-03-02 23:23:30 +0100

    Update license

--- winkeys/winkeys.py
+++ winkeys/winkeys.py
@@ -1,6 +1,27 @@
-import os
-
 """
+ -----------------------------------------------------------------------------
+ MIT License
+
+ Copyright (c) 2019 Daniel Månsson
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ -----------------------------------------------------------------------------
 Provides a simple API for performing mouse and keyboard operations in Python for Windows.
 This implementation using the ctypes library does not require the application itself to be in focus when performing its
 operations and can therefore be ran in the background.
@@ -15,6 +36,8 @@ Does not (really) support:
 - Writing capitalized letters (can be done by toggling 'caps') 
 - Writing special symbols (can be made by holding down the correct modifier buttons)
 """
+
+import os
 
 timeout: int = 5  # ms
 
